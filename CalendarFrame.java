6a32aa99420165746fe90b1e0935cc71d037bd8d
Keara <kearatar@gmail.com>
2018-10-29 10:12:46 -0300

    Add files via upload

--- CalendarFrame.java
+++ CalendarFrame.java
@@ -14,39 +14,40 @@ import java.awt.*;
 
 public class CalendarFrame 
 {
-   public static void main(String[] args)
-   {
-     // Do not change any of the dimensions (at least the x's). Bad things will happen.
-     MonthYearPanel monthp = new MonthYearPanel();
-     monthp.setPreferredSize(new Dimension(100,100));
+	public static void main()
+	{
+		// Do not change any of the dimensions (at least the x's). Bad things will happen.
+		MonthYearPanel monthp = new MonthYearPanel();
+		monthp.setPreferredSize(new Dimension(100,100));
      
-     DayPanel dayp = new DayPanel();
-     dayp.setPreferredSize(new Dimension(100,100));      
+		DayPanel dayp = new DayPanel();
+		dayp.setPreferredSize(new Dimension(100,100));      
 
-     CalendarPanel calp = new CalendarPanel();
-     calp.setPreferredSize(new Dimension(500,400));//500 300
-      
-     //getting the placements of the buttons from the DayPanel.
-      int[] placements = new int[7];
-      placements = dayp.getPlacements();
-      //Applying the above placements to the buttons in the CalendarPanel.
-      calp.doButtonPlacements(placements);
-      
-      JFrame frame = new JFrame("ScheduleMeCalendar");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-      // Setting the layout of the main frame.
-      frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-      
-      //Adding the panels in different sections of the content pane.
-      frame.getContentPane().add(monthp);
-      frame.getContentPane().add(dayp);
-      frame.getContentPane().add(calp);
+		CalendarPanel calp = new CalendarPanel();
+		calp.setPreferredSize(new Dimension(500,400));//500 300
+		
+		//getting the placements of the buttons from the DayPanel.
+		int[] placements = new int[7];
+		placements = dayp.getPlacements();
+		//Applying the above placements to the buttons in the CalendarPanel.
+		calp.doButtonPlacements(placements);
+		
+		JFrame frame = new JFrame("ScheduleMeCalendar");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+		// Setting the layout of the main frame.
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-      frame.setPreferredSize(new Dimension(985,600));
+		//Adding the panels in different sections of the content pane.
+		frame.getContentPane().add(monthp);
+		frame.getContentPane().add(dayp);
+		frame.getContentPane().add(calp);
+
+		frame.setPreferredSize(new Dimension(985,600));
       
-      // The Window won't be resizeable to allow for good button placement.
-      frame.setResizable(false);
-      frame.pack();
-      frame.setVisible(true);
-   }
+		// The Window won't be resizeable to allow for good button placement.
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 }
