73f4cd6907c3e0d90f51155d0833ff563d28e7dc
Keara <kearatar@gmail.com>
2018-10-29 23:34:10 -0300

    Add files via upload

--- convert.java
+++ convert.java
@@ -9,89 +9,92 @@ import java.util.*;
 
 public class convert
 {
-    public boolean[][] dateArray = new boolean[7][36];
-    public String dates = new String();
-    public int count = 0;
+	public boolean[][] dateArray = new boolean[7][36];
+	public String dates = new String();
+	public int count = 0;
 
-    public void convert()
-    {
-      dates = new String();
-    }
+	public void convert()
+	{
+		dates = new String();
+	}
 
-    /*Name: convertString
-     *parameters: String parameter of length 252
-     *returns: 7 by 36 boolean array
-     *
-     *description: converts String to a 7 by 36 array
-     */
-    
- public boolean[][] convertString(String data)
- {   
-     int len = data.length();
-     if(len != 252)
-     {
-       System.out.println("invalid number of input!");
-       System.exit(0);
-     }
-     char part;
-     char tru = '1';
-     char fal = '0';
-     
-     for(int i = 0; i < 7; i++)
-       for(int j = 0; j < 36; j++)
-     {
-       part = data.charAt(count);
-   
-       if(part == tru)
-         dateArray[i][j] = true;
-       else if(part == fal)
-         dateArray[i][j] = false;
-       else
-       {
-         System.out.println("invalid input detected!");
-         System.exit(0);
-       }
-       
-       count++;
-     }
-     
-     return dateArray;
- }
+	/*Name: convertString
+	 *parameters: String parameter of length 252
+	 *returns: 7 by 36 boolean array
+	 *
+	 *description: converts String to a 7 by 36 array
+	 */
 
-    /*Name: convertArray
-     *parameters: 7 by 36 boolean array
-     *returns: String parameter of length 252
-     *
-     *description: converts 7 by 36 array to a  String
-     */
- public String convertArray(boolean[][] booldateArray)
- {
-     for(int i = 0; i < 7; i++)
-       for(int j = 0; j < 36; j++)
-     {
-       if(booldateArray[i][j] == true)
-         dates += "1";
-       else if(booldateArray[i][j] == false)
-       {
-         //System.out.println("The date is false");
-         dates += "0";
-       }
-       else
-       {
-         System.out.println("invalid value in array detected!");
-         System.exit(0);
-       }
-      }
-      
-     return dates;
- }
- public void backend(boolean [][] array, String name, String path)
-  {
-    //convert newCon = new convert();
-    String returnString = convertArray(array);
-    System.out.println(returnString);
-    
-    SamsFunctions func = new SamsFunctions(name, returnString, path);
-    func.addEntry(name, returnString, path); 
-  }
+	public boolean[][] convertString(String data)
+	{   
+		int len = data.length();
+		if(len != 252)
+		{
+			System.out.println("invalid number of input!");
+			System.exit(0);
+		}
+		char part;
+		char tru = '1';
+		char fal = '0';
+
+		for(int i = 0; i < 7; i++)
+			for(int j = 0; j < 36; j++)
+			{
+				part = data.charAt(count);
+
+				if(part == tru)
+					dateArray[i][j] = true;
+				else if(part == fal)
+					dateArray[i][j] = false;
+				else
+				{
+					System.out.println("invalid input detected!");
+					System.exit(0);
+				}
+
+				count++;
+			}
+
+		return dateArray;
+	}
+
+	/*Name: convertArray
+	 *parameters: 7 by 36 boolean array
+	 *returns: String parameter of length 252
+	 *
+	 *description: converts 7 by 36 array to a  String
+	 */
+	public String convertArray(boolean[][] booldateArray)
+	{
+		for(int i = 0; i < 7; i++)
+			for(int j = 0; j < 36; j++)
+			{
+				if(booldateArray[i][j] == true)
+					dates += "1";
+				else if(booldateArray[i][j] == false)
+				{
+					//System.out.println("The date is false");
+					dates += "0";
+				}
+				else
+				{
+					System.out.println("invalid value in array detected!");
+					System.exit(0);
+				}
+			}
+
+		return dates;
+	}
+	public void backend(boolean [][] array, String name, String path)
+	{
+		//convert newCon = new convert();
+		String returnString = convertArray(array); // convert from true/false array to 1s&0s
+		System.out.println(returnString);	// debug stmt
+
+		SamsFunctions func = new SamsFunctions(name, returnString, path);
+		func.addEntry(name, returnString, path); 
+		
+		if (path != null)
+			func.calculate(path);
+	}
 }
--- gui_init.java
+++ gui_init.java
@@ -11,104 +11,106 @@ import java.awt.event.*;
 import java.awt.Color;
 
 public class gui_init extends JFrame implements ActionListener {
- 
- private static final long serialVersionUID = 1L;
- private JLabel lbQuestion1, lbQuestion2, lbExistingFile;
- private JButton btUploadFile, btSubmit;
- private Color lime = new Color(129, 196, 43);
- private Color maroon = new Color(234, 98, 98);
- public static String filePath;
- public String returnString;
- public void createView() {
-  
-  JPanel p = new JPanel();
-  getContentPane().add(p);
-  
-  // Question
-  lbQuestion1 = new JLabel("To get started, upload an existing text file");
-  p.add(lbQuestion1);
-  lbQuestion2 = new JLabel("or submit without one to create a new session.");
-  p.add(lbQuestion2);
 
-  // existing file label updates when file selected
-  lbExistingFile = new JLabel("Upload a file");
-  lbExistingFile.setForeground(maroon);
-  lbExistingFile.setSize(100,30);
-  p.add(lbExistingFile);
-  
-  // Upload Existing
-  btUploadFile = new JButton("Upload");
-  p.add(btUploadFile);
-  // Upload file action
-  btUploadFile.addActionListener((ActionListener) this);
-  
-  // Submit all
-  btSubmit = new JButton("Submit");
-  p.add(btSubmit);
-  
-  // Submit action
-  btSubmit.addActionListener((ActionListener) this);
- }
- 
- private void updateFileName(String fileName) {
-   lbExistingFile.setForeground(lime);
-   lbExistingFile.setText(fileName);
- }
- 
- // If setup is 1 it will display the intro frame. 
- // If not, you can just use gui_init's functions.
- gui_init(int setup) 
- {
-   if (setup == 1)
-   {
-  createView();
-  
-  // General defaults
-  setTitle("ScheduleMe");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setLocationRelativeTo(null);
-        setSize(410,160);
-        setVisible(true);
-    }
- }
- // ActionEvent handler
- public void actionPerformed(ActionEvent e) {
-  // Upload a file
-  if (e.getSource() == btUploadFile) {
-   JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-   jfc.setDialogTitle("Select a .json file");
-   jfc.setAcceptAllFileFilterUsed(false);
-   FileNameExtensionFilter filter = new FileNameExtensionFilter("*.json", "json");
-   jfc.addChoosableFileFilter(filter);
+	private static final long serialVersionUID = 1L;
+	private JLabel lbQuestion1, lbQuestion2, lbExistingFile;
+//	public tfName;
+	private JButton btUploadFile, btSubmit;
+	private Color lime = new Color(129, 196, 43);
+	private Color maroon = new Color(234, 98, 98);
+	public static String filePath;
+	public String returnString;
+	public void createView() {
 
-   int returnValue = jfc.showOpenDialog(null);
-   if (returnValue == JFileChooser.APPROVE_OPTION) {
-    filePath = jfc.getSelectedFile().getPath();
-    System.out.println(jfc.getSelectedFile().getName());
-    updateFileName(jfc.getSelectedFile().getName());
-   }
-  }
-  // Submit and run calendar
-  if (e.getSource() == btSubmit) {
-   setVisible(false);
-   CalendarFrame.main();
-  }
- }
- 
- public static String getPath(){
-  return filePath; 
- }
- 
- 
+		JPanel p = new JPanel();
+		getContentPane().add(p);
 
- // main() method
- public static void main(String[] args) {
-  SwingUtilities.invokeLater(new Runnable() {
-   @Override
-   public void run() {
-    new gui_init(1);
-   }
-  });
- }
+		// Question
+		lbQuestion1 = new JLabel("To get started, upload an existing text file");
+		p.add(lbQuestion1);
+		lbQuestion2 = new JLabel("or submit without one to create a new session.");
+		p.add(lbQuestion2);
+
+		// existing file label updates when file selected
+		lbExistingFile = new JLabel("Upload a file");
+		lbExistingFile.setForeground(maroon);
+		lbExistingFile.setSize(100,30);
+		p.add(lbExistingFile);
+
+		// Upload Existing
+		btUploadFile = new JButton("Upload");
+		p.add(btUploadFile);
+		// Upload file action
+		btUploadFile.addActionListener((ActionListener) this);
+
+		// Submit all
+		btSubmit = new JButton("Submit");
+		p.add(btSubmit);
+
+		// Submit action
+		btSubmit.addActionListener((ActionListener) this);
+	}
+
+	private void updateFileName(String fileName) {
+		lbExistingFile.setForeground(lime);
+		lbExistingFile.setText(fileName);
+	}
+
+	// If setup is 1 it will display the intro frame. 
+	// If not, you can just use gui_init's functions.
+	gui_init(int setup) 
+	{
+		if (setup == 1)
+		{
+			createView();
+
+			// General defaults
+			setTitle("ScheduleMe");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			pack();
+			setLocationRelativeTo(null);
+			setSize(410,160);
+			setVisible(true);
+		}
+	}
+	// ActionEvent handler
+	public void actionPerformed(ActionEvent e) {
+		// Upload a file
+		if (e.getSource() == btUploadFile) {
+			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			jfc.setDialogTitle("Select a .json file");
+			jfc.setAcceptAllFileFilterUsed(false);
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("*.json", "json");
+			jfc.addChoosableFileFilter(filter);
+
+			int returnValue = jfc.showOpenDialog(null);
+			if (returnValue == JFileChooser.APPROVE_OPTION) {
+				System.out.println(jfc.getSelectedFile().getPath());
+				filePath = jfc.getSelectedFile().getPath();
+				System.out.println(jfc.getSelectedFile().getName());
+				// update JPanel with filename
+				updateFileName(jfc.getSelectedFile().getName());
+			}
+		}
+		// Submit and run calendar
+		if (e.getSource() == btSubmit) {
+			setVisible(false);
+			CalendarFrame.main();
+		}
+	}
+
+	// a way to get filepath from anywhere
+	public static String getPath(){
+		return filePath; 
+	}
+
+	// main() method
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new gui_init(1);
+			}
+		});
+	}
 }
