68ea8bacb1f2d346584fc8f0135dd7e61f9e2788
Keara <kearatar@gmail.com>
2018-10-30 01:50:51 -0300

    Add files via upload
    Fix typo

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /*
  * File Name:	Main.java
  * Author:		Keara Leibovitz
- * Purpose:		Runs gui_init. Wchi in turn runs CalendarFrame on Submit.
+ * Purpose:		Runs gui_init. Which in turn runs CalendarFrame on Submit.
  * 				Which in turn runs gui_final to display results.
  * Date:		October 30, 2018
  */
